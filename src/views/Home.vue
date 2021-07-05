0ab7ca038952631647d77c49f66f0d26597561db
Doppelganker <rim.dars.97@gmail.com>
2021-07-05 03:00:13 +0200

    Changed logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,6 @@
   <div class="flex flex-col items-center gap-24">
     <p class="text-5xl text-blue-800 text-center mt-10">Velika igra</p>
     <router-link class="btn bg-blue-500 hover:bg-blue-400 w-40 h-40 flex items-center justify-center text-xl" to="/1">Začni</router-link>
-    <img src="../assets/logo.png" />
+    <img src="../assets/logo.jpg" width="450" />
   </div>
 </template>
